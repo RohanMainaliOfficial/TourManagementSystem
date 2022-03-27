804d8c6e4c61a0d88b5fd9065419f50ad83824a6
RohanKumarMainali <rohanmainali39@gmail.com>
2022-03-27 21:30:07 +0545

    Building CRUD API

--- tour/base/models.py
+++ tour/base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+class Packages(models.Model):
+    id=models.IntegerField(primary_key=True)
+    name=models.CharField(max_length=150)
+    description=models.CharField(max_length=500)
 
+    def __str__(self):
+        return self.name
 # Create your models here.
--- tour/base/views.py
+++ tour/base/views.py
@@ -1,9 +1,54 @@
 
 from django.shortcuts import render
-def index(request):
-    return render(request,'index.html')
+from .models import Packages
+from  . import serialize
+from .serialize import PackageSerializer
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-def admin(request):
-    return render(request,'admin.html')
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls={
+        'Packages':"/Packages/",
+        'Detail View':"/package-details/",
+        'Delete':'/package-delete/<str:pk>/',
+        'Update':'/package-update/<str:pk>/',
+        'Create':'/package-create/',
+    }
+    return Response(api_urls)
+@api_view(['GET'])
+def package_list(request):
+    Package=Packages.objects.all()
+    serializeObj=PackageSerializer(Package,many='true')
+    return Response(serializeObj.data)
 
-# Create your views here.
+@api_view(['GET'])
+def package_detail(request, pk):
+    Package=Packages.objects.get(id=pk)
+    serializeObj=PackageSerializer(Package,many='false')
+    return Response(serializeObj.data)
+
+@api_view(['POST'])
+def create_package(request):
+    serializeObj=PackageSerializer(data=request.data)
+    if serializeObj.is_valid():
+        serializeObj.save()
+    return Response(serializeObj.data)
+
+@api_view(['POST'])
+def update_package(request,pk):
+    Package=Packages.objects.get(id=pk)
+    serializeObj=PackageSerializer(instance=Package,data=request.data)
+    if serializeObj.is_valid():
+        serializeObj.save()
+    return Response(serializeObj.data)
+
+@api_view(['DELETE'])
+def delete_package(request,pk):
+    Package=Packages.objects.get(id=pk)
+    Package.delete()
+
+    return Response("Package Deleted Successfully")
+
+
