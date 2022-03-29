e8335523d2b8917705d723558adacc3de00dc04a
developer <prabesh065@gmail.com>
2022-03-29 10:39:04 +0545

    update

--- tour/frontend/src/App.js
+++ tour/frontend/src/App.js
@@ -1,46 +1,14 @@
+
 import './App.css';
-import profile from "./../image/a.png";
-import email from "./../image/email.jpg";
-import pass from "./../image/pass.png";
-function LoginUi() {
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbarcomp from './components/Navbarcomp';
+
+function App() {
   return (
-    <div className="main">
-     <div className="sub-main">
-       <div>
-         <div className="imgs">
-           <div className="container-image">
-             <img src={profile} alt="profile" className="profile"/>
-
-           </div>
-
-
-         </div>
-         <div>
-           <h1>Login Page</h1>
-           <div>
-             <img src={email} alt="email" className="email"/>
-             <input type="text" placeholder="user name" className="name"/>
-           </div>
-           <div className="second-input">
-             <img src={pass} alt="pass" className="email"/>
-             <input type="password" placeholder="user name" className="name"/>
-           </div>
-          <div className="login-button">
-          <button>Login</button>
-          </div>
-
-            <p className="link">
-              <a href="#">Forgot password ?</a> Or<a href="#">Sign Up</a>
-            </p>
-
-
-         </div>
-       </div>
-
-
-     </div>
+    <div className='App'>
+      <Navbarcomp/> 
     </div>
   );
 }
 
-export default LoginUi;
+export default App;
