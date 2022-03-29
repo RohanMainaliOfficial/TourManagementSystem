8a938ab51904935d42e54bb2c38f3c51352675c3
RohanKumarMainali <rohanmainali39@gmail.com>
2022-03-29 13:22:51 +0545

    Fetching and Posting on API

--- tour/tour/settings.py
+++ tour/tour/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig',
     'tour',
     'rest_framework',
+    'corsheaders',
 
 ]
 
@@ -40,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'tour.urls'
@@ -116,3 +118,9 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR,'frontend/build/static')]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+
+    "http://localhost:3000",
+
+]
