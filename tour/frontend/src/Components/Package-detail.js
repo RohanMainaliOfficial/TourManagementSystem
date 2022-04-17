6b220772a210275bdd1ec6bc2d0f08d7b685fe7c
developer <prabesh065@gmail.com>
2022-04-17 13:04:32 +0545

    sprint_1

--- tour/frontend/src/Components/Package-detail.js
+++ tour/frontend/src/Components/Package-detail.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useLocation} from 'react-router-dom';
-import {package_item} from './Navbarcomp'
+import {package_item} from './Navbarcomp';
 import './ViewPackage.css';
 
 function Package_detail(){
