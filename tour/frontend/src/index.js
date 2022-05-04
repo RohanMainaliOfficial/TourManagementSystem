5e7d96f8be8f1887292c5470c5b835dff81c61ec
Biraj <np03cs4s210303@heraldcollege.edu.np>
2022-05-04 18:44:53 +0545

    booking form

--- tour/frontend/src/index.js
+++ tour/frontend/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import {BrowserRouter,Link,Navigate,Route,Routes} from 'react-router-dom';
 import './Components/Style.css';
 import AddForm from './Components/AddForm';
 import Navbarcomp from './Components/Navbarcomp';
-import Update from './Components/Update';
+import Update from './Components/Updat  e';
 import Package_detail from './Components/Package-detail';
 import Login from "./admin_login/login";
 import ProtectedRoute from './Components/ProtectedRoute';
-
+import  Booking_info from './Components/Book';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -20,11 +20,13 @@ ReactDOM.render(
 
         <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Login/>} />
-                    <Route path="/home" element=<ProtectedRoute element={<Navbarcomp/>} />  />
-                    <Route path="/add" element=<ProtectedRoute element={<AddForm/>} />  />
-                    <Route path="/update" element=<ProtectedRoute element={<Update/>} />  />
-                    <Route path="/package_details" element=<ProtectedRoute element={<Package_detail/>} />  />
+                   <Route path="/" element={ <Navigate to="/login" />} />
+                    <Route path="/login" element={<Login/>} />
+                    <Route path="/home" element=< ProtectedRoute element={<Navbarcomp/>} />  />
+                    <Route path="/add" element=< ProtectedRoute element={<AddForm/>} />  />
+                    <Route path="/update" element=< ProtectedRoute element={<Update/>} />  />
+                    <Route path="/package_details" element=< ProtectedRoute element={<Package_detail/>} />  />
+                    <Route path="/book" element={<Booking_info/>} />
                 </Routes>
         </BrowserRouter>
   </React.StrictMode>,
